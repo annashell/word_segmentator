eb802bbbf837101d7a6247fca49440f2e7365b4b
AnnaShell <anna.tityushina@yandex.ru>
2024-10-10 17:29:03 +0300

    detect pauses

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from scipy.special import other
-
 from sig_analysis import detect_pauses, detect_fricative_parts
-from utils.signal_classes import Signal, Seg
+from utils.signal_classes import Signal, Seg, Label
 
 
 def process_signal(signal: Signal) -> list:
@@ -9,9 +7,9 @@ def process_signal(signal: Signal) -> list:
     detects cluster boundaries in audio
     writes seg-file with word boundaries labels
     """
-    labels = []
+    labels = [Label(0, "Y1", 'begin')]
     labels = detect_pauses(signal, labels)
-    labels = detect_fricative_parts(signal, labels)
+    #labels = detect_fricative_parts(signal, labels)
 
     return labels
 
@@ -19,13 +17,35 @@ def process_signal(signal: Signal) -> list:
 def process_text(text: str) -> (list, list):
     """
     detects cluster boundaries in text
+    0 - vowels
+    1 - sonorants
+    2 - plosives
+    3 - other consonants
+    4 - pauses
     """
     vowels = ('а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я')
     sonorants = ('й', 'л', 'м', 'н', 'р')
     plosives = ('п', 'б', 'т', 'д', 'к', 'г')
     other = ('в', 'ж', 'з', 'с', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь')
-    clusters = []
+
+    clusters = [(0, 9)]
     word_boundaries_indexes = []
+
+    for i, ch in enumerate(text.lower().strip()):
+        if ch.isspace():
+            word_boundaries_indexes.append(i)
+        elif ch in ('.', ',', ':', ';'):
+            clusters.append((i, 4))
+        elif ch in vowels and clusters[-1][1] != 0:
+            clusters.append((i, 0))
+        elif ch in sonorants and clusters[-1][1] != 1:
+            clusters.append((i, 1))
+        elif ch in plosives and clusters[-1][1] != 2:
+            clusters.append((i, 2))
+        elif ch in other and clusters[-1][1] != 3:
+            clusters.append((i, 3))
+
+    clusters = clusters[1:]
     return clusters, word_boundaries_indexes
 
 
@@ -38,18 +58,34 @@ def make_alignment(signal_clusters, text_clusters, word_boundaries_indexes) -> l
     return word_boundaries
 
 
-def main(wav_fn, text) -> None:
+def main(wav_fn, text_fn) -> None:
     """
     writes seg-file with word boundaries
     :param wav_fn: audio filename
     :param text: correspondent text
     """
+    text = ""
+    with open(text_fn, encoding="utf-8") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        text += line.strip()
+
     signal = Signal(wav_fn)
+    signal.read_sound_file()
     ac_labels = process_signal(signal)
-    txt_clusters, word_boundaries_indexes = process_text(text)
-    word_boundaries = make_alignment(ac_labels, txt_clusters, word_boundaries_indexes)
+    # txt_clusters, word_boundaries_indexes = process_text(text)
+    # word_boundaries = make_alignment(ac_labels, txt_clusters, word_boundaries_indexes)
 
     new_seg_fn = wav_fn.split(".")[0] + ".seg_Y1"
-    new_seg = Seg(new_seg_fn, word_boundaries, signal.params)
+    # new_seg = Seg(new_seg_fn, word_boundaries, signal.params)
+    new_seg = Seg(new_seg_fn, ac_labels, signal.params)
     new_seg.write_seg_file()
     print(f"Границы слов записаны в файл {new_seg_fn}")
+
+
+wav_fn =  r"D:\pycharm_projects\word_segmentator\test_data\cta0004.wav"
+text_fn = r"D:\pycharm_projects\word_segmentator\test_data\cta0004.txt"
+
+
+main(wav_fn, text_fn)
--- sig_analysis.py
+++ sig_analysis.py
@@ -1,8 +1,32 @@
-from utils.signal_classes import Signal
+from cProfile import label
+
+import numpy as np
+from scipy.fft import rfft
+
+from utils.signal_classes import Signal, Label, level2code
 
 
 def detect_pauses(signal: Signal, labels: list = []):
-    pass
+    #TODO: проверить согласные после паузы в дебаге
+    N = int(0.02 * signal.params.samplerate // signal.params.sampwidth) # окно анализа 20 мс в отсчетах
+    max_signal_ampl = max([np.abs(i) for i in signal.signal])
+    for i in range (len(signal.signal) // N):
+        signal_part = signal.signal[i * N : i * N + N]
+        max_part_ampl = max([np.abs(i) for i in signal_part])
+        new_label_position = int(i * N)
+        to_prev_label_time_distance = (new_label_position - labels[-1].position) / signal.params.samplerate
+        if max_part_ampl < 0.07 * max_signal_ampl:
+            if labels[-1].text != 'pause':
+                if to_prev_label_time_distance > 0.15 or labels[-1].text == 'begin':
+                    labels.append(Label(new_label_position, "Y1", 'pause'))
+                elif len(labels) > 2:
+                    labels.pop(len(labels) - 1)
+        elif labels[-1].text != 'new_synt':
+            if to_prev_label_time_distance > 0.15 or labels[-1].text == 'begin':
+                labels.append(Label(new_label_position, "Y1", 'new_synt'))
+            elif len(labels) > 2:
+                labels.pop(len(labels) - 1)
+    return labels
 
 
 def detect_fricative_parts(signal: Signal, labels: list = []):
