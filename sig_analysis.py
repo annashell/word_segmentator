4d4438e9cbeeb00f46671cc6e1c2e16da63098ed
AnnaShell <anna.tityushina@yandex.ru>
2024-10-14 17:06:16 +0300

    fictive pause in the beginning for correct processing of the first syntagma

--- sig_analysis.py
+++ sig_analysis.py
@@ -9,19 +9,22 @@ def detect_pauses(signal: Signal, labels: list = [], config: dict = {}):
 
     N = int(config["pause_detection_parameters"]["window_size"] * signal.params.samplerate // signal.params.sampwidth) # окно анализа 20 мс в отсчетах
     max_signal_ampl = max([np.abs(i) for i in signal.signal])
-    for i in range (len(signal.signal) // N):
-        signal_part = signal.signal[i * N : i * N + N]
+    fictive_pause = [0 for i in range(
+        int(config["pause_detection_parameters"]["min_pause_duration"] * signal.params.samplerate * 2))]
+    signal_ = fictive_pause + list(signal.signal)
+    for i in range (len(signal_) // N):
+        signal_part = signal_[i * N : i * N + N]
         max_part_ampl = max([np.abs(i) for i in signal_part])
-        new_label_position = int(i * N)
+        new_label_position = int(i * N - len(fictive_pause))
         to_prev_label_time_distance = (new_label_position - labels[-1].position) / signal.params.samplerate
         if max_part_ampl < config["pause_detection_parameters"]["threshold"] * max_signal_ampl:
             if labels[-1].text != 'pause':
-                if to_prev_label_time_distance > config["pause_detection_parameters"]["max_stop_duration"] or labels[-1].text == 'begin':
+                if to_prev_label_time_distance > config["pause_detection_parameters"]["min_pause_duration"] or labels[-1].text == 'begin':
                     labels.append(Label(new_label_position, "Y1", 'pause'))
                 elif len(labels) > 2:
                     labels.pop(len(labels) - 1)
         elif labels[-1].text != 'new_synt':
-            if to_prev_label_time_distance > config["pause_detection_parameters"]["max_stop_duration"] or labels[-1].text == 'begin':
+            if to_prev_label_time_distance > config["pause_detection_parameters"]["min_pause_duration"] or labels[-1].text == 'begin':
                 labels.append(Label(new_label_position, "Y1", 'new_synt'))
             elif len(labels) > 2:
                 labels.pop(len(labels) - 1)
