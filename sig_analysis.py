14b8a135351833f9081043295833af5de03b8739
AnnaShell <anna.tityushina@yandex.ru>
2024-11-11 15:22:30 +0300

    pause detection upgr

--- sig_analysis.py
+++ sig_analysis.py
@@ -1,17 +1,120 @@
 import math
 from copy import deepcopy
 
+import numpy as np
 from scipy import signal as sc_signal
 from scipy.fft import rfft, rfftfreq
 
-from utils.signal_classes import Signal, Label
+from utils.json_utils import get_object_from_json
+from utils.signal_classes import Signal, Label, Seg
+
+
+def get_zeros(wav_signal: list, N_wind):
+  sig_len = len(wav_signal)
+  res = int(sig_len % (N_wind // 2))
+  num_zeros = int(N_wind / 2 - res)
+  wav_signal.extend(np.zeros(num_zeros))
+  return wav_signal
+
+
+def detect_pauses_2(filename: str, labels: list = [], config: dict = {}):
+    wav_signal = Signal(filename)
+    wav_signal.read_wav()
+
+    wav_signal_ = list(wav_signal.signal)
+
+    config_ = config["pause_detection_parameters"]
+    N_wind = int(config_["window_size"] * wav_signal.params.samplerate // wav_signal.params.sampwidth)
+
+    # дополняем сигнал нулями
+    wav_signal_ = get_zeros(wav_signal_, N_wind)
+
+    number_of_windows = len(wav_signal_) // N_wind
+
+    # 1. Вычисляем интенсивность сигнала
+    intensity = []
+    for i in range(number_of_windows):
+        start = i * N_wind
+        end = start + N_wind
+        sig_part = np.array(wav_signal_[start:end])
+
+        # Вычисляем среднюю мощность сигнала в окне
+        squared_signal = sig_part ** 2
+        intensity.append(np.mean(squared_signal))
+
+    print(max(intensity), min(intensity), np.mean(intensity), np.log(min(intensity)) / np.log(max(intensity)))
+    max_intensity = max(intensity)
+
+    # 2. Интервалы выше порога определяем как речевые, ниже - как паузы
+    threshold = 0.6
+    for i, int_wind in enumerate(intensity):
+        if np.log(int_wind) / np.log(max_intensity) > threshold:
+            labels.append(Label(i * N_wind, "B1", "voiced"))
+        else:
+            labels.append(Label(i * N_wind, "B1", "pause"))
+
+    # 3. Объединяем речевые и неречевые интервалы
+    labels_united = [labels[0]]
+    for label1, label2 in zip(labels, labels[1:]):
+        if label2.text != label1.text:
+            labels_united.append(label2)
+
+    # 4. Удаляем короткие паузы (скорее всего, это смычные согласные)
+    labels_united_cleared = []
+    indexes_to_delete = []
+    n = 1
+    for label1, label2 in zip(labels_united[1:], labels_united[2:]):
+        if label1.text == "voiced" and (label2.position - label1.position) / wav_signal.params.samplerate < 0.05: # min_alloph_duration:
+            if n < len(labels_united) - 1:
+                indexes_to_delete.append(n)
+                indexes_to_delete.append(n + 1)
+        n += 1
+
+    for i, label in enumerate(labels_united):
+        if i not in indexes_to_delete:
+            labels_united_cleared.append(label)
+
+    # 5. Удаляем короткие звучащие сегменты (это не речевые звуки)
+    labels_united_cleared_2 = []
+    indexes_to_delete_2 = []
+    n = 1
+    for label1, label2 in zip(labels_united_cleared[1:], labels_united_cleared[2:]):
+        if label1.text == "voiced" and (label2.position - label1.position) / wav_signal.params.samplerate < 0.05: # min_alloph_duration:
+            if n < len(labels_united_cleared) - 1:
+                indexes_to_delete_2.append(n)
+                indexes_to_delete_2.append(n + 1)
+        n += 1
+
+    for i, label in enumerate(labels_united_cleared):
+        if i not in indexes_to_delete:
+            labels_united_cleared_2.append(label)
+
+    # 6. Пишем seg
+    new_seg_fn = filename.split(".")[0] + ".seg_B1"
+    new_seg = Seg(new_seg_fn, labels_united_cleared_2, wav_signal.params)
+    new_seg.write_seg_file()
+    print(f"Границы слов записаны в файл {new_seg_fn}")
+
+
+
+
+
+config = get_object_from_json(r"D:\pycharm_projects\word_segmentator\config.json")
+detect_pauses_2(r"D:\pycharm_projects\word_segmentator\test_data\av15t.wav", [], config)
 
 
 def detect_pauses(signal: Signal, labels: list = [], config: dict = {}):
+    """
+
+    :param signal:
+    :param labels:
+    :param config:
+    :return:
+    """
     config_ = config["pause_detection_parameters"]
 
     N = int(
-        config_["window_size"] * signal.params.samplerate // signal.params.sampwidth)  # окно анализа 20 мс в отсчетах
+        config_["window_size"] * signal.params.samplerate // signal.params.sampwidth)  # окно анализа в отсчетах
     max_signal_ampl = max([abs(i) for i in signal.signal])
     fictive_pause = [0 for i in range(
         int(config_["min_pause_duration"] * signal.params.samplerate * 2))]
