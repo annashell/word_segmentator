be837a98d614f3bc130ab8585c254fd35a0510ab
AnnaShell <anna.tityushina@yandex.ru>
2024-12-04 17:00:05 +0300

    syntagma alignment algo fixes

--- data/bruteforce_alignment_algo.py
+++ data/bruteforce_alignment_algo.py
@@ -22,37 +22,43 @@ def make_str_alignment(string_1, string_2, count=0):
 
     for i, ch in enumerate(list(string_1)):
 
+        if string_2.startswith(string_1):
+            return len(string_2) - len(string_1)
+
+        if string_1.startswith(string_2):
+            return len(string_1) - len(string_2)
+
         if ch == string_2[i]:
             continue
 
         edit_variants = []
 
-        str1_with_del_symb = delete_same_symbols(string_1[:i] + string_1[i:])
+        str1_with_del_symb = delete_same_symbols(string_1[:i] + string_1[i + 1:])
         levenstein_dist_del = nltk.edit_distance(str1_with_del_symb, string_2)
 
         edit_variants.append(("del", levenstein_dist_del))
 
         possible_symb_to_replace = [x for x in possible_symb if x != ch]
         for symb in possible_symb_to_replace:
-            str1_with_repl_symb = delete_same_symbols(string_1[:i] + symb + string_1[i:])
+            str1_with_repl_symb = delete_same_symbols(string_1[:i] + symb + string_1[i + 1:])
             levenstein_dist_repl = nltk.edit_distance(str1_with_repl_symb, string_2)
-            edit_variants.append(("r", levenstein_dist_repl))
+            edit_variants.append(("r", levenstein_dist_repl, symb))
 
         for symb in possible_symb_to_replace:
-            str1_with_inserted_symb = delete_same_symbols(string_1[:i + 1] + symb + string_1[i:])
+            str1_with_inserted_symb = delete_same_symbols(string_1[:i] + symb + string_1[i + 1:])
             levenstein_dist_repl = nltk.edit_distance(str1_with_inserted_symb, string_2)
-            edit_variants.append(("i", levenstein_dist_repl))
+            edit_variants.append(("i", levenstein_dist_repl, symb))
 
         distances = [x[1] for x in edit_variants]
         best_var = edit_variants[distances.index(min(distances))]
         if best_var[0] == 'del':
             new_str = str1_with_del_symb
-        elif best_var[1] == 'r':
-            new_str = delete_same_symbols(string_1[:i] + best_var[0] + string_1[i:])
-        elif best_var[1] == 'i':
-            new_str = delete_same_symbols(string_1[:i + 1] + best_var[0] + string_1[i:])
+        elif best_var[0] == 'r':
+            new_str = delete_same_symbols(string_1[:i] + best_var[2] + string_1[i:])
+        elif best_var[0] == 'i':
+            new_str = delete_same_symbols(string_1[:i + 1] + best_var[2] + string_1[i:])
         count += 1
-        return make_str_alignment(new_str, str2, count)
+        return make_str_alignment(new_str, string_2, count)
 
 
 def partition(arr, n):
@@ -140,10 +146,10 @@ txt_clusters = [(0, 0), (2, 1), (3, 0), (12, 2), (13, 0), (17, 1), (18, 2), (19,
 # print(mods)
 
 
-# make_most_probable_syntagma_distribution(str1, txt_clusters, word_boundaries_ind)
+make_most_probable_syntagma_distribution(str1, txt_clusters, word_boundaries_ind)
 
 
-make_str_alignment("0120", "010")
+# make_str_alignment("0120", "010")
 
 
 
