591d338d5496322fbe11120d1a574fd2ef6b5850
AnnaShell <anna.tityushina@yandex.ru>
2024-12-25 17:12:41 +0300

    фиксики

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import re
 from copy import copy
 from difflib import ndiff
 
+from matplotlib.pyplot import pause
+
 from alignment_algo import make_most_probable_syntagma_distribution, \
     make_most_probable_syntagma_distribution_2
 from sig_analysis import detect_pauses, detect_allophone_types
@@ -119,7 +121,6 @@ def process_text(text: str, for_syntagmas) -> (list, list):
     word_boundaries_indexes = [0]
 
     txt_arr = latin_txt.lower().strip().split()
-    # TODO звонкие мягкие смычные = период + шум
     for i, ch in enumerate(txt_arr):
         if ch == '0':
             word_boundaries_indexes.append(i)  # -1 из-за границы слова в начале
@@ -130,8 +131,9 @@ def process_text(text: str, for_syntagmas) -> (list, list):
         elif ch in stops and (clusters[-1][1] != 2 or (i != 0 and txt_arr[i - 1] == "0")):
             clusters.append((i, 2))
         elif ch in affricates and (clusters[-1][1] != 2 or (i != 0 and txt_arr[i - 1] == "0")):
-            clusters.append((i, 2))
-            clusters.append((i, 1))
+            if txt_arr[i - 1] not in affricates:
+                clusters.append((i, 2))
+                clusters.append((i, 1))
         elif ch in other and (clusters[-1][1] != 3 or (i != 0 and txt_arr[i - 1] == "0")):
             if for_syntagmas:
                 clusters.append((i, 0))
@@ -154,10 +156,6 @@ lbl_to_str_dict = {
 }
 
 
-def nearest(lst, target):
-    return min(lst, key=lambda x: abs(x - target))
-
-
 def convert_ac_labels_to_string(ac_labels):
     ac_string = ""
     for label in ac_labels:
@@ -166,170 +164,14 @@ def convert_ac_labels_to_string(ac_labels):
     return ac_string
 
 
-def convert_txt_clusters_to_string(text_clusters):
-    txt_str = "4"
-    for cluster in text_clusters:
-        txt_str += str(cluster[1])
-    return txt_str
-
-
-def make_alignment(ac_parts, text_parts, word_boundaries_indexes) -> list:
-    """
-    makes text and audio labels alignment
-    :return list of dicts, where key is a word, value - start and end of the word in audio
-    """
-    ac_parts_with_space = {}
-
-    for i, part in enumerate(ac_parts):
-        ac_string = convert_ac_labels_to_string(part)
-        text_part = text_parts[i]
-        txt_string = convert_txt_clusters_to_string(text_part)
-        str_difference = list(ndiff(ac_string, txt_string))
-
-        # индексы текстовых кластеров с пробелами
-        spaced_parts_indexes = []
-        for ind in word_boundaries_indexes:
-            for cl1, cl2 in zip(text_part, text_part[1:]):
-                if ind == cl2[0] - 1:
-                    spaced_parts_indexes.append(text_part.index(cl2))
-                elif cl1[0] <= ind < cl2[0]:
-                    spaced_parts_indexes.append(text_part.index(cl1))
-
-        ac_part_labels = []
-        for label in part:
-            if label.level == "R1":
-                ac_part_labels.append(label)
-
-        cnt = -1
-        plus_count = 0
-        for j, x in enumerate(str_difference):
-            if x.startswith('+'):
-                plus_count += 1
-            if not x.startswith('-'):
-                cnt += 1
-                if cnt in spaced_parts_indexes:
-                    number_of_spaces = spaced_parts_indexes.count(cnt)
-                    ac_spaced_part_index = j - plus_count + 1
-                    ac_part_with_space = ac_part_labels[ac_spaced_part_index]
-                    ac_part_with_space_st = ac_part_with_space.position
-                    if ac_part_with_space_st not in ac_parts_with_space.keys():
-                        ac_parts_with_space[ac_part_with_space_st] = number_of_spaces
-                    else:
-                        ac_parts_with_space[ac_part_with_space_st] += number_of_spaces
-
-        ac_parts_with_space[part[-1].position] = 1
-        if i < len(ac_parts) - 1:
-            ac_parts_with_space[ac_parts[i + 1][0].position] = 1
-
-    return ac_parts_with_space
-
-
-def split_acoustic_labels(ac_labels):
-    acoustic_parts = []
-    new_part = []
-    for label in ac_labels:
-        if label.text == "pause":
-            acoustic_parts.append(new_part)
-            new_part = []
-        else:
-            new_part.append(label)
-    acoustic_parts = list(filter(lambda x: len(x) > 0, acoustic_parts))
-    return acoustic_parts
-
-
-def split_text_clusters(txt_clusters):
-    text_parts = []
-    new_text_part = []
-    for cluster in txt_clusters:
-        if cluster[1] == 4:
-            text_parts.append(new_text_part)
-            new_text_part = []
-        else:
-            new_text_part.append(cluster)
-    text_parts = list(filter(lambda x: len(x) > 0, text_parts))
-    return text_parts
-
-
-def define_word_boundaries(ac_parts_with_space, ac_labels, words):
-    space_labels = []
-    cnt = 0
-
-    filtered_ac_labels = list(filter(lambda x: x.position in ac_parts_with_space.keys(), ac_labels))
-
-    for label, next_label in zip(filtered_ac_labels, filtered_ac_labels[1:]):
-        if label.position in ac_parts_with_space.keys():
-            if cnt >= len(words):
-                break
-            number_of_spaces = ac_parts_with_space[label.position]
-            cluster_duration = next_label.position - label.position
-            for i in range(number_of_spaces):
-                space_labels.append(
-                    Label(label.position + (i + 1) * (cluster_duration // (number_of_spaces + 1)), "B1", words[cnt]))
-                cnt += 1
-
-    ac_labels.extend(space_labels)
-    return ac_labels
-
-
-def define_syntagmas(ac_labels, txt_clusters, text):
-    ac_part_labels = []
-    for label in ac_labels:
-        if label.level == "R1":
-            ac_part_labels.append(label)
-
-    ac_string = convert_ac_labels_to_string(ac_part_labels)
-    text_string = convert_txt_clusters_to_string(txt_clusters)
-    str_difference = list(ndiff(text_string, ac_string))
-
-    alignments = pairwise2.align.globalxx(text_string.split(),
-                                          ac_string.split(),
-                                          gap_char=['-'])
-
-    format_alignment(*alignments[0])
-
-    # индексы текстовых кластеров, после которых идет пауза
-    spaced_parts_indexes = []
-    without_sign_count = 0
-    for dif in str_difference:
-        if not dif.startswith(("+", "-")):
-            without_sign_count += 1
-        if dif == "+ 4":
-            spaced_parts_indexes.append(without_sign_count)
-
-    pause_indexes = [txt_clusters[x][0] for x in spaced_parts_indexes]
-
-    word_boundaries_indexes = []
-    for i, ch in enumerate(list(text)):
-        if ch == " ":
-            word_boundaries_indexes.append(i)
-
-    pause_indexes_final = [0]
-    for ind in pause_indexes:
-        pause_indexes_final.append(nearest(word_boundaries_indexes, ind))
-    pause_indexes_final.append(len(text))
-
-    text_fragments = []
-    for ind1, ind2 in zip(pause_indexes_final, pause_indexes_final[1:]):
-        text_frag = text[ind1: ind2]
-        text_fragments.append(text_frag)
-
-    lbl_count = 0
-    for label in ac_labels:
-        if label.level == "Y1" and label.text in ("begin", "new_synt"):
-            label.text = text_fragments[lbl_count]
-            lbl_count += 1
-
-    return ac_labels
-
-
-def unite_txt_clusters(txt_clusters_synt_new):
+def unite_txt_clusters(txt_clusters_synt_new, unite_by_position=False):
     txt_clusters_synt_new_un = []
     delete_next = False
     for cl1, cl2 in zip(txt_clusters_synt_new, txt_clusters_synt_new[1:]):
         if delete_next:
             delete_next = False
             continue
-        if cl2[1] == cl1[1]:
+        if cl2[1] == cl1[1] or (unite_by_position and cl2[0] == cl1[0] and  cl2[1] != cl1[1]): # для двухчастных согласных
             txt_clusters_synt_new_un.append(cl1)
             delete_next = True
         else:
@@ -339,8 +181,7 @@ def unite_txt_clusters(txt_clusters_synt_new):
     return txt_clusters_synt_new_un
 
 
-def rewrite_acoustic_markers(best_syntagma_distribution, best_operations, ac_labels, text, txt_clusters):
-    new_ac_markers_all = []
+def rewrite_acoustic_markers(best_syntagma_distribution, best_operations, ac_labels, txt_arr, txt_clusters):
     new_txt_distribution = []
 
     for synt_count in range(len(best_operations)):
@@ -351,29 +192,70 @@ def rewrite_acoustic_markers(best_syntagma_distribution, best_operations, ac_lab
         if len(operations) == 0:
             new_txt_distribution.extend(txt_clusters_synt)
         else:
+            insertions_count = 0
             for (operation, index, ch) in operations:
                 txt_clusters_synt_new = copy(txt_clusters_synt)
+                index = index + insertions_count
                 if operation == 'd':
-                    to_del_el = txt_clusters_synt[index]
+                    to_del_el = txt_clusters_synt_new[index]
                     txt_clusters_synt_new = txt_clusters_synt_new[:index] + txt_clusters_synt_new[index + 1:]
-                    txt_clusters_synt_new[index] = (to_del_el[0], txt_clusters_synt_new[index][1])
+                    txt_clusters_synt_new.insert(index, (to_del_el[0], txt_clusters_synt_new[index][1]))
+                    insertions_count += 1
                     txt_clusters_synt_new = unite_txt_clusters(txt_clusters_synt_new)
                 elif operation == 'r':
                     txt_clusters_synt_new[index] = (txt_clusters_synt_new[index][0], int(ch))
                     txt_clusters_synt_new = unite_txt_clusters(txt_clusters_synt_new)
                 elif operation == 'i':
-                    txt_clusters_synt_new = txt_clusters_synt_new[:index] + [(txt_clusters_synt_new[index][0], int(ch))] + txt_clusters_synt_new[index:]
+                    txt_clusters_synt_new = txt_clusters_synt_new[:index] + [
+                        (txt_clusters_synt_new[index][0], int(ch))] + txt_clusters_synt_new[index:]
                     txt_clusters_synt_new = unite_txt_clusters(txt_clusters_synt_new)
+
+            txt_clusters_synt_new = unite_txt_clusters(txt_clusters_synt_new, True)
 
             new_txt_distribution.extend(txt_clusters_synt_new)
 
-    ac_labels_count = 0
-    for label in ac_labels:
-        if label.level == 'R1' and label.text != "":
-            label_text = text[new_txt_distribution[ac_labels_count][0] : new_txt_distribution[ac_labels_count+1][0]]
-            ac_labels.append(Label(label.position, "R2", label_text))
-            ac_labels_count += 1
+    pause_starts = [x[1] for x in best_syntagma_distribution]
 
+    # spaces_to_insert = []
+    # for cl1, cl2 in zip(txt_clusters, txt_clusters[1:]):
+    #     for pause_start in pause_starts:
+    #         if cl1[0] < pause_start < cl2[0]:
+    #             spaces_to_insert.append((txt_clusters.index(cl2) + len(spaces_to_insert), (pause_start, cl1[1])))
+    #
+    # for (index, new_label) in spaces_to_insert:
+    #     new_txt_distribution.insert(index, new_label)
+
+    ac_labels_count = 0
+    prev_label = ""
+    new_ac_labels = []
+
+    for i, label in enumerate(ac_labels):
+        if label.level == 'R1' and label.text != "":
+            label_text = " ".join(
+                txt_arr[new_txt_distribution[ac_labels_count][0]: new_txt_distribution[ac_labels_count + 1][0]])
+
+            label_text = re.sub(r'[\d.,!:;-]', '', label_text)
+            new_ac_labels.append(Label(label.position, "R2", label_text))
+            if label_text in ('p', 't', 'k') and prev_label == "" and label.text != "voiceless_stop":
+                ac_labels_count += 1
+                next_label_text = " ".join(
+                txt_arr[new_txt_distribution[ac_labels_count][0]: new_txt_distribution[ac_labels_count + 1][0]])
+                next_label_text = re.sub(r'[\d.,!:;-]', '', next_label_text)
+                new_ac_labels.append(Label(label.position + 512, "R2", next_label_text))
+            ac_labels_count += 1
+            prev_label = label.text
+
+    move_index = 0
+    for label1, label2 in zip(new_ac_labels, new_ac_labels[1:]):
+        affricates = ("p'", "k'", "t'", "d'", "c")
+        if label1.text.strip() in affricates and label2.text.strip() in affricates:
+            label1.text = label1.text.strip() + " " + label2.text.strip()
+            label1.position = new_ac_labels[new_ac_labels.index(label1) + move_index].position
+            move_index += 1
+        else:
+            label1.text = new_ac_labels[new_ac_labels.index(label1) + move_index].text
+
+    ac_labels.extend(new_ac_labels)
     return ac_labels
 
 
@@ -394,18 +276,20 @@ def define_syntagmas_2(ac_labels, txt_clusters, text, word_boundaries_indexes, s
         if label1.text == "new_synt":
             ac_synt_durations.append((label2.position - label1.position) / sampling_freq)
 
-    best_syntagma_distribution, best_operations_all = make_most_probable_syntagma_distribution_2(ac_string, txt_clusters,
-                                                                            word_boundaries_indexes, ac_synt_durations,
-                                                                            txt_arr)
+    best_syntagma_distribution, best_operations_all = make_most_probable_syntagma_distribution_2(ac_string,
+                                                                                                 txt_clusters,
+                                                                                                 word_boundaries_indexes,
+                                                                                                 ac_synt_durations,
+                                                                                                 txt_arr)
     count = 0
     for label in ac_labels:
         if label.level == "Y1" and label.text == "new_synt":
             synt_text = text[best_syntagma_distribution[count][0]: best_syntagma_distribution[count][-1]]
             label.text = synt_text
             count += 1
-    
-    rewrite_acoustic_markers(best_syntagma_distribution, best_operations_all, ac_labels, text, txt_clusters)
-            
+
+    rewrite_acoustic_markers(best_syntagma_distribution, best_operations_all, ac_labels, txt_arr, txt_clusters)
+
     return ac_labels
 
 
